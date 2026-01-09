f106ffa30b6e040f42faf7ce89804c92e4f58d9f
AgriKwik <agriKwik@gmail.com>
2026-01-10 01:23:02 +0200

    First commit

--- app/core/database.py
+++ app/core/database.py
@@ -1,20 +1,21 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Render/SQLAlchemy fix for 'postgres://' vs 'postgresql://'
+
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
     DATABASE_URL,
-    # --- ADD THESE THREE LINES ---
     pool_size=10,
     max_overflow=20,
-    pool_recycle=300, # Discard connections older than 5 mins
-    pool_pre_ping=True # Checks if connection is alive before using it
+    pool_recycle=300,
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
